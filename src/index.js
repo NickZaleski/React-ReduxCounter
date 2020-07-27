21bd2ff5e451c45b46281590b8c244610ba7e820
Nick <mikalailivanov@gmail.com>
2020-07-27 14:47:15 +0300

    counter made with redux and casual html/css

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const reducer = (state = 0, action) => {
+    switch (action.type){
+      case 'INC': 
+        return state + 1;
+      case 'DEC':
+        return state - 1;
+      case 'RES':
+        return state = 0;
+      default:
+        return state;
+    }
+   
+}
+
+const store = createStore(reducer);
+
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const res = (value) => ({type: 'RES', value});
+
+
+
+document.getElementById('inc').addEventListener('click', (e) => {
+    store.dispatch(inc())
+});
+
+document.getElementById('dec').addEventListener('click', (e) => {
+  store.dispatch(dec())
+});
+
+document.getElementById('res').addEventListener('click', (e) => {
+  const value = 0;
+  store.dispatch(res(value));
+});
+
+
+const update = () => {
+  document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(update);
+   
